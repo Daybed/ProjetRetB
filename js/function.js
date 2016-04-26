02cad77c988cfc358d314e92214f1166316b9c97
Daybed <davidchaille@hotmail.fr>
2016-04-26 19:40:36 +0200

    Intégration bower_components et petites modifs

---
+++ js/function.js
@@ -0,0 +1,219 @@
+var somme =0;
+var k;
+var newtab=[{etat:false},{etat:false},{etat:false},{etat:false}];
+var conf = JSON.parse(fs.readFileSync('conf.json'));
+
+function initHue(callback){
+  var tab=[];
+  var rep = JSON.parse(Get('http://'+conf.ipAdresseHue+'/api/'+conf.hueUsername+'/lights/'));
+
+    for(i in rep){
+        if (rep[i].state.reachable==true){
+            var lampe = {lampe : i, on : rep[i].state.on, bri : rep[i].state.bri, xy: [rep[i].state.xy[0],rep[i].state.xy[1]], hue:rep[i].state.hue, sat:rep[i].state.sat};
+            tab.push(lampe);
+        }
+        else{
+        }
+    }
+    callback(tab);
+};
+
+function init(socket){
+initHue(function(hue){
+socket.emit('initHue',hue);
+});
+}
+
+
+function Get(url) {
+    xmlHttpGet.open( "GET", url , false ); 
+    xmlHttpGet.send( null );
+        if(xmlHttpGet.status==200){
+            return xmlHttpGet.responseText;
+        }
+}
+
+function Put(url,paramASend){
+    xmlHttpPut.open("PUT", url, false ); 
+    xmlHttpPut.send(paramASend);
+        if(xmlHttpPut.status==200){
+            return xmlHttpPut.responseText;
+        }
+}
+
+ function getIPAddress() {
+  var interfaces = require('os').networkInterfaces();
+  for (var devName in interfaces) {
+    var iface = interfaces[devName];
+
+    for (var i = 0; i < iface.length; i++) {
+      var alias = iface[i];
+      if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal)
+        return alias.address;
+    }
+  }
+  return '0.0.0.0';
+}
+
+
+  function getall(){
+    for(var i in light){
+    getknx(light[i].adresse);
+}
+  } 
+  function connectionknx(callback){
+    connection.Connect(function (){callback();});
+  }
+  function deconnectionknx(callback){
+    connection.Disconnect(function(){callback();});
+  }
+  function setknx(adresse,value){
+    connection.Action(adresse,value);
+  }
+  function getknx(adresse){
+    connection.RequestStatus(adresse);
+  }
+
+
+function exec(callback){
+
+      if(chenillard.clockwise==true){
+
+              for (var j=0; j<light.length;j++){
+
+               k = (j+1+light.length) % light.length;
+               somme+=light[j].etat;
+
+                  if(light[j].etat==1){
+                    newtab[k].etat=true;
+                  }
+                  else{
+                    newtab[k].etat=false;
+                  }
+                }
+              }
+
+        else if(chenillard.clockwise==false){
+
+            for (var j=0; j<light.length;j++){
+
+              k = (j-1+light.length) % light.length;
+              somme+=light[j].etat;
+
+              if(light[j].etat==1){
+                newtab[k].etat=true;
+              }
+              else{
+                  newtab[k].etat=false;
+                }
+                
+            }
+          }
+
+
+        if(somme==light.length || somme==0){
+          console.log("Pas d'action à effectuer, lampes toute allumées ou lampes toute éteintes");
+          //toast
+        }
+        
+        else if(chenillard.on==true){
+            for(var i in newtab){
+              setknx(light[i].adresse,newtab[i].etat);
+            }
+        }
+
+        else{
+          console.log("Erreur");
+        }
+
+        somme = 0;
+        callback();
+}
+
+function looptest (){
+
+    if(chenillard.on==true){
+      exec(function(){
+      setTimeout(function(){
+        looptest();}, chenillard.speed
+      );
+    });
+    }
+    else{
+      return;
+    }
+}
+
+
+function rgb2xy(R,G,B){
+
+  var X = 0.4124*R + 0.3576*G + 0.1805*B;
+  var Y = 0.2126*R + 0.7152*G + 0.0722*B;
+  var Z = 0.0193*R + 0.1192*G + 0.9505*B;
+  var coorx = X / (X + Y + Z);
+  var coory = Y / (X + Y + Z);
+  return {x:coorx,y:coory};
+
+}
+
+function rgb2hsl (r,g,b) {
+  var r1=r/255;
+  var g1=g/255;
+  var b1=b/255;
+ var minRGB = Math.min(r1,Math.min(g1,b1));
+ var maxRGB = Math.max(r1,Math.max(g1,b1));
+ var delta=maxRGB-minRGB;
+ var l;
+ var s;
+ l = (minRGB + maxRGB)/2;
+ if(delta==0){
+   s = 0;
+}
+else{
+  s = delta/(1-(Math.abs(2*l -1)));
+}
+s= Math.round(s*255);
+l = Math.round(l*255);
+return {sat:s,bri:l};
+}
+function rgb2hsv (r,g,b) {
+ var computedH = 0;
+ var computedS = 0;
+ var computedV = 0;
+
+ //remove spaces from input RGB values, convert to int
+ var r = parseInt( (''+r).replace(/\s/g,''),10 ); 
+ var g = parseInt( (''+g).replace(/\s/g,''),10 ); 
+ var b = parseInt( (''+b).replace(/\s/g,''),10 ); 
+
+ if ( r==null || g==null || b==null ||
+     isNaN(r) || isNaN(g)|| isNaN(b) ) {
+   alert ('Please enter numeric RGB values!');
+   return;
+ }
+ if (r<0 || g<0 || b<0 || r>255 || g>255 || b>255) {
+   alert ('RGB values must be in the range 0 to 255.');
+   return;
+ }
+ r=r/255; g=g/255; b=b/255;
+ var minRGB = Math.min(r,Math.min(g,b));
+ var maxRGB = Math.max(r,Math.max(g,b));
+
+ // Black-gray-white
+ if (minRGB==maxRGB) {
+  computedV = minRGB;
+  return [0,0,computedV];
+ }
+
+ // Colors other than black-gray-white:
+ var d = (r==minRGB) ? g-b : ((b==minRGB) ? r-g : b-r);
+ var h = (r==minRGB) ? 3 : ((b==minRGB) ? 1 : 5);
+ computedH = 60*(h - d/(maxRGB - minRGB));
+ computedS = Math.round(((maxRGB - minRGB)/maxRGB)*255);
+ computedV = Math.round(maxRGB*255);
+
+
+ return {h:computedH,s:computedS,v:computedV};
+}
+
+// voir xy2rgb
---
+++ public/js/client.js
@@ -0,0 +1,199 @@
+var app = angular.module("myApp",['ngMaterial','ngToast']);
+var ip;
+var socket=io();
+var Lampes=[];
+
+function couleur(picker){
+
+  var res=["","",""];
+  var k = 0;
+  for(i in picker.style.backgroundColor){
+    if(i>3 && i<picker.style.backgroundColor.length-1){
+      if(picker.style.backgroundColor[i]==","){
+        res[k]=parseInt(res[k]);
+        k+=1;
+      }
+      else{
+      res[k]+=picker.style.backgroundColor[i];
+    }
+  }
+  }
+
+ var resultat = {lampe : 2, r: parseInt(res[0]), g: parseInt(res[1]), b: parseInt(res[2])};
+ console.log(resultat);
+ socket.emit('color',resultat);
+};
+ 
+
+app.controller('myCtrl', function($scope,$http,ngToast) {
+  $scope.HueImg="public/img/hue.png";
+  $scope.fond="#ffffff";
+ /* $scope.slider={
+    value: 0,
+    min:0,
+    max:255,
+    options:{
+      floor:0,
+      ceil:1
+    }
+  };
+
+
+  $scope.couleur = function(picker){
+    console.log(picker.rgb);
+  };
+
+<button onclick="add()">Add 100 pickers</button>
+
+<p id="container"></p>
+
+<script>
+function add() {
+    for(var i = 0; i < 100; i++) {
+        var input = document.createElement('INPUT')
+        var picker = new jscolor(input)
+        picker.fromHSV(360 / 100 * i, 100, 100)
+    
+        document.getElementById('container').appendChild(input)
+    }
+}
+</script>*/
+
+    socket.on('init',function(data){
+      ip = data.ipserver;
+      $scope.speed=data.chenillardspeed;
+
+      $scope.$apply(function () {
+       if (data.chenillardstate==true){
+        $scope.loopstart="Stop chenillard";
+       }
+       else{
+        $scope.loopstart="Start chenillard";
+       }
+     });
+    });
+    
+
+     socket.on('lampes',function(data){
+          for(i in data){
+            if(data[i].etat==1){
+              Lampes[i]={img:"bower_components/material-design-icons/action/drawable-hdpi/ic_lightbulb_outline_black_on_48dp.png",adresse:data[i].adresse,etat:true, num: data[i].numero}; 
+            }
+            else if(data[i].etat==0){
+              Lampes[i]={img:"bower_components/material-design-icons/action/drawable-hdpi/ic_lightbulb_outline_black_off_48dp.png",adresse:data[i].adresse,etat:false, num:data[i].numero};
+            }
+            else{
+              Lampes[i]={img:"bower_components/material-design-icons/action/drawable-hdpi/ic_lightbulb_outline_black_error_48dp.png",adresse:data[i].adresse,etat:"error", num: data[i].numero};
+        
+          }
+        }
+          $scope.$apply(function(){$scope.lampes=Lampes;});
+      });
+       
+
+
+     socket.on('etat chenillard',function(data){
+        $scope.$apply(function () {
+              if (data==true){
+              $scope.loopstart="Stop chenillard";
+              }
+              else{
+              $scope.loopstart="Start chenillard";
+              }
+        });
+     });
+
+     $scope.loop=function(){
+      socket.emit('changestate');
+     };
+
+    $scope.changedirection=function(){
+      socket.emit('changedirection');
+     };
+
+     $scope.setspeed = function(){
+     socket.emit('setspeed',$scope.vitesse);
+   }; 
+
+    $scope.lampe= function(numero){
+      if(Lampes[numero-1].etat==="error"){
+        ngToast.create({
+         content: "Erreur. Cette lampe ne fonctionne pas ou n'est pas branchée, veuillez utiliser les autres.",
+         dismissOnTimeout : true,
+         timeout: 3000,
+         className: 'danger',
+        });
+      }
+      else{
+      socket.emit('setlampe',{adresse:Lampes[numero-1].adresse,etat:!Lampes[numero-1].etat});
+     }
+    };
+
+    socket.on('speedchenillard',function(vitesse){
+      $scope.speed=vitesse;
+    });
+
+    socket.on('initHue',function(data){
+        console.log(data);
+        $scope.$apply(function(){
+        $scope.Hue=data;
+      });
+    
+    });
+
+    
+
+    $scope.changeonhue = function(numero){
+      for(i in $scope.Hue){
+        if($scope.Hue[i].lampe == numero){
+          socket.emit('on',{lampe:numero,on:!$scope.Hue[i].on});
+        }
+      }
+      
+    };
+
+    $scope.changebrihue=function(numero){
+       for(i in $scope.Hue){
+        if($scope.Hue[i].lampe == numero){
+          socket.emit('bri',{lampe:numero,bri:$scope.Hue[i].bri});
+        }
+      }
+      
+     };
+
+  
+  $scope.changesathue=function(numero){
+       for(i in $scope.Hue){
+        if($scope.Hue[i].lampe == numero){
+          socket.emit('sat',{lampe:numero,sat:$scope.Hue[i].sat});
+        }
+      }
+      
+     };
+
+  /*socket.on('ChangementOnHue',function(data){
+      for (i in $scope.Hue){
+        if($scope.Hue[i].lampe==data.lampe){
+          $scope.$apply(function(){
+            $scope.Hue[i].on=data.on;
+          });
+        }
+        else{
+        }
+      }
+
+  });
+
+  socket.on('ChangementBriHue',function(data){
+      for (i in data){
+        console.log("xy : " + data[i].xy +", bri :" + data[i].bri);
+      }
+  });
+
+  socket.on('ChangementSatHue',function(data){
+      console.log(data);
+  });
+*/
+
+
+});
